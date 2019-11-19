0edea59092623b03029009d795129b494a8677d2
xxX-Avrack-Xxx <vitekvin@seznam.cz>
2019-11-19 17:09:48 +0100

    The code (bruteforce) is completely functional. Bugs -> Fixed 'a' issue      -> 4 warnings

--- main.c
+++ main.c
@@ -2,78 +2,48 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-/*
-void loginPlace (char login[], char count[], int position[]) {
-    //int *position = malloc(12);
 
-    //projede kazdou pozici pole
-    for (int i=0; i<12; i++) {
-        //pohleda cely Count
-        for (int x=0; x<62; x++) {
-            //pokud se to bude rovnat tak se to ulozi
-            if (login[i]==count[x]) {
-                position[i]=x;
-                //printf("%c %d - %d\n",login[i],x, position[i]);
-            }
+void nextPlace(char login[], char count[], int place[]) {
+    int x = 1;
+
+    for (int i = 0; i < x; i++) {
+
+        if (login[i] == count[61]) {
+            login[i] = count[0];
+            place[i] = 0;
+            x++;
+        } else {
+            login[i] = count[place[i] + 1];
+            place[i]++;
         }
     }
 }
-*/
 
-void nextPlace (char login[], char count[], int place[], int lenghtOfCount) {
-    //pomocna promenna -> pokud se predesle cislo rovna 61 (posledni znak)
-    //tak aura se bude rovnat 1 -> posune rad v dalsim posunu loginu
-    int aura=0;
+void setup(char login[], char count[], int place[]) {
 
-    for (int i=11; i>=0; i--) {
-
-        if (i==11) {
-            if (login[i]==count[lenghtOfCount-1]) {
-                login[i]=count[0];
-                place[i]=0;
-                aura=1;
-
-            } else {
-                login[i]=count[place[i] +1];
-                place[i]++;
-            }
-        }
-        else if (aura==1) {
-            if (login[i]==count[lenghtOfCount-1]) {
-                login[i]=count[0];
-                place[i]=0;
-                aura=1;
-
-            } else {
-                login[i]=count[place[i] +1];
-                place[i]++;
-            }
-        }
+    for (int i = 0; i < 12; i++) {
+        place[i] = -1;
+        login[i] = 0;
     }
+    login[0] = count[0];
+    place[0] = 0;
 }
 
 int main() {
     //abeceda
     char count[63]={'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
                     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-                    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0','/0 '};
+                    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '\0 '};
 
     //umisteni
     int place[12];
     char login[12];
 
-    for(int e=0; e<13; ++e) {
-        login[e]=0;
-        place[e]=0;
-    }
-    login[11]='a';
-    place[11]=0;
+    setup(login, count, place);
 
-    while(1){
-        nextPlace(login,count,place,63);
+    while (1) {
+        nextPlace(login, count, place);
         printf("%s\n", login);
-        //sleep(1);
     }
-
     return 0;
 }
